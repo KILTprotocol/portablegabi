f85c6c8366b6cdaa076ddef0e2e0f9c3af67d4b9
wischli <w.freude@icloud.com>
2020-01-10 17:37:20 +0100

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: '../jest.env.js',
+  clearMocks: true,
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  collectCoverageFrom: ['**/*.ts', '!index.ts'],
+  rootDir: 'src',
+  coverageDirectory: '../coverage',
+}
