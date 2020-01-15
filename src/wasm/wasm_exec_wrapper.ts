00bcd0a46ef8fb99178380b8651d4b6a5263ff6b
wischli <w.freude@icloud.com>
2020-01-15 13:47:40 +0100

    fix: memory issue by multiple wasm instantiations

--- src/wasm/wasm_exec_wrapper.ts
+++ src/wasm/wasm_exec_wrapper.ts
@@ -1,10 +1,24 @@
 import WasmHooks from './WasmHooks'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const goWasm = require('./wasm_exec')
+const goWasm = require('./new_wasm_exec')
+// const goWasm = require('./test')
+
+const GoInstance = goWasm.GoWasm.init()
 
 const goWasmExec = <T>(
   goHook: WasmHooks,
   args?: string[] | number[]
-): Promise<T> => goWasm.exec(goHook, args)
+): Promise<T> =>
+  Promise.resolve(GoInstance).then(wasm => wasm.execWasmFn(goHook, args))
+
+export const goWasmExecCustom = <T>(
+  goHook: WasmHooks,
+  customGoInstance: any,
+  args?: string[] | number[]
+): Promise<T> => customGoInstance.execWasmFn(goHook, args)
+
+export const goWasmClose = (): Promise<void> =>
+  Promise.resolve(GoInstance).then(wasm => wasm.close())
+
 export default goWasmExec
