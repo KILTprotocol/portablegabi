45c28a65e16aa6f12d54b3e16711930d43351a34
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-02-07 15:53:22 +0100

    doc: add jsdoc to GabiAttester

--- src/attestation/GabiAttester.ts
+++ src/attestation/GabiAttester.ts
@@ -1,3 +1,6 @@
+/**
+ * This module contains the GabiAttester Class which is used to create and revoke attestations.
+ */
 import { AttestationRequest } from '../types/Claim'
 import goWasmExec from '../wasm/wasm_exec_wrapper'
 import WasmHooks from '../wasm/WasmHooks'
@@ -12,32 +15,61 @@ import IGabiAttester, {
   AttesterPrivateKey,
 } from '../types/Attestation'
 
+/**
+ * The GabiAttester can be used to create and revoke attestations.
+ */
 export default class GabiAttester implements IGabiAttester {
   private readonly privateKey: AttesterPrivateKey
   readonly publicKey: AttesterPublicKey
 
-  // generate keypair
-  public static async genKeyPair(): Promise<{
+  /**
+   * Generates a new key pair.
+   *
+   * @param validityDuration The duration for which the public key will be valid.
+   * @param maxAttributes The maximal number of attributes that can be signed with the generated private key.
+   * @returns A newly generated key pair.
+   */
+  public static async genKeyPair(
+    validityDuration: number,
+    maxAttributes: number
+  ): Promise<{
     privateKey: AttesterPrivateKey
     publicKey: AttesterPublicKey
   }> {
-    const validityDuration = 365 * 24 * 60 * 60 * 1000 * 1000 * 1000 // 365 days in nanoseconds
     const { privateKey, publicKey } = await goWasmExec<{
       privateKey: string
       publicKey: string
-    }>(WasmHooks.genKeypair, [70, validityDuration])
+    }>(WasmHooks.genKeypair, [maxAttributes, validityDuration])
     return {
       privateKey: new AttesterPrivateKey(privateKey),
       publicKey: new AttesterPublicKey(publicKey),
     }
   }
 
-  // create new instance
-  public static async create(): Promise<GabiAttester> {
-    const { publicKey, privateKey } = await this.genKeyPair()
+  /**
+   * Generates a new key pair and returns a new attester.
+   *
+   * @param validityDuration The duration for which the public key will be valid.
+   * @param maxAttributes The maximal number of attributes that can be signed with the generated private key.
+   * @returns A new Attester.
+   */
+  public static async create(
+    validityDuration: number,
+    maxAttributes: number
+  ): Promise<GabiAttester> {
+    const { publicKey, privateKey } = await this.genKeyPair(
+      validityDuration,
+      maxAttributes
+    )
     return new GabiAttester(publicKey, privateKey)
   }
 
+  /**
+   * Constructs a new attester using the private and public key.
+   *
+   * @param publicKey The public key for the attester.
+   * @param privateKey The private key for the attester.
+   */
   public constructor(
     publicKey: AttesterPublicKey,
     privateKey: AttesterPrivateKey
@@ -46,7 +78,12 @@ export default class GabiAttester implements IGabiAttester {
     this.privateKey = privateKey
   }
 
-  // start attestation
+  /**
+   * Initiates the attestation session.
+   *
+   * @returns A session and a message object.
+   *    The message should be send over to the claimer.
+   */
   public async startAttestation(): Promise<{
     message: InitiateAttestationRequest
     session: AttesterAttestationSession
@@ -61,6 +98,11 @@ export default class GabiAttester implements IGabiAttester {
     }
   }
 
+  /**
+   * Creates a new accumulator.
+   *
+   * @returns A new [[Accumulator]].
+   */
   public async createAccumulator(): Promise<Accumulator> {
     return new Accumulator(
       await goWasmExec<string>(WasmHooks.createAccumulator, [
@@ -70,7 +112,15 @@ export default class GabiAttester implements IGabiAttester {
     )
   }
 
-  // issue attestation
+  /**
+   * Creates an attestation for the claim inside the [[AttestationRequest]].
+   *
+   * @param p The parameter object.
+   * @param p.attestationSession The attestation session which was generated during [[startAttestation]].
+   * @param p.attestationRequest The AttestationRequest received from the claimer.
+   * @param p.update The most recent [[Accumulator]].
+   * @returns The [[Attestation]] object which should be send to the claimer and a witness which can be used to revoke the attestation.
+   */
   public async issueAttestation({
     attestationSession,
     attestationRequest,
@@ -99,7 +149,14 @@ export default class GabiAttester implements IGabiAttester {
     }
   }
 
-  // revoke attestation
+  /**
+   * Revokes an [[Attestation]] which corresponds to the provided [[Witness]].
+   *
+   * @param p The parameter object.
+   * @param p.update The current [[Accumulator]].
+   * @param p.witness The [[Witness]] belonging to the [[Attestation]] which is about to be revoked.
+   * @returns An updated version of the [[Accumulator]].
+   */
   public async revokeAttestation({
     accumulator,
     witnesses,
