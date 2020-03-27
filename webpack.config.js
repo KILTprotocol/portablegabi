7a4160cf7394236aa8e21816fc1da6b18f5ce981
wischli <w.freude@icloud.com>
2020-03-27 12:54:28 +0100

    chore: add yarn webpack alias

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   module: {
     rules: [
       {
-        // test: /\.tsx?$/,
-        // use: 'ts-loader',
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
