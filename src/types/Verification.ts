907617913e42b09b923a38b39e05f32b4e5ced8c
wischli <w.freude@icloud.com>
2020-04-28 10:22:06 +0200

    docs: add js-doc for getRequestedProperties

--- src/types/Verification.ts
+++ src/types/Verification.ts
@@ -32,6 +32,12 @@ export class VerificationSession extends WasmData {
  * The message result of [[requestPresentation]] which should be sent to the [[Claimer]] and used in [[buildPresentation]].
  */
 export class PresentationRequest extends WasmData {
+  /**
+   * Returns the attributes a [[Verifier]] requested during [[requestPresentation]].
+   *
+   * @throws `Invalid request` If the parsed object of this [[PresentationRequest]] does not have the correct structure (missing mandatory properties).
+   * @returns An array of strings representing the requested attributes of a [[Claimer]]'s claim.
+   */
   public getRequestedProperties(): string[] {
     const parsed = this.parse()
     if ('partialPresentationRequest' in parsed) {
@@ -56,6 +62,12 @@ export class CombinedVerificationSession extends WasmData {
  * The message result of [[requestCombinedPresentation]] which should be sent to the [[Claimer]] and used in [[buildCombiendPresentation]].
  */
 export class CombinedPresentationRequest extends WasmData {
+  /**
+   * Returns the attributes a [[Verifier]] requested during [[requestCombinedPresentation]].
+   *
+   * @throws `Invalid request` If the parsed object of this [[CombinedPresentationRequest]] does not have the correct structure (missing mandatory properties).
+   * @returns An array of array of strings representing the requested attributes of a [[Claimer]]'s claim.
+   */
   public getRequestedProperties(): string[][] {
     const parsed = this.parse()
     if ('partialPresentationRequests' in parsed) {
